898ad4c265bbc07b69b3c25cd5b566f7dffeaf14
hsnconsult <42925803+hsnconsult@users.noreply.github.com>
2019-05-29 11:05:20 +0000

    Update __manifest__.py

--- convl/__manifest__.py
+++ convl/__manifest__.py
@@ -2,17 +2,15 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Decimal Precision Configuration',
+    'name': 'convl',
     'description': """
-Configure the price accuracy you need for different kinds of usage: accounting, sales, purchases.
+conversion
 =================================================================================================
 """,
     'version': '0.1',
     'depends': ['base'],
     'category' : 'Hidden',
     'data': [
-        'views/decimal_precision_views.xml',
-        'security/ir.model.access.csv',
     ],
     'demo': [],
     'installable': True,
