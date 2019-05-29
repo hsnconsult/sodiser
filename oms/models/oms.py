df597c5ddf8f6bcad7b3eb76c9f480395124aae3
hsnconsult <42925803+hsnconsult@users.noreply.github.com>
2019-05-29 10:52:07 +0000

    Update oms.py

--- oms/models/oms.py
+++ oms/models/oms.py
@@ -12,7 +12,7 @@ from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from odoo.exceptions import UserError, ValidationError
 from odoo import api, fields, models, _
 from odoo.osv import osv
-from . import convlettres
+
 
 
 
